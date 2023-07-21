6f2985318562f0182ac3d657a91718afbc71ec8b
kkkovalov <vlad_kovalov@icloud.com>
2023-07-20 19:16:02 -0700

    templates updated

--- chat_app/base/admin.py
+++ chat_app/base/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Room)
+admin.site.register(models.Message)
+admin.site.register(models.Topic)
