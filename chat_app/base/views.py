4c932d2fda9737fb1b41aef8f944ba5576c51607
kkkovalov <vlad_kovalov@icloud.com>
2023-07-20 19:07:59 -0700

    Added models and changed templates layout

--- chat_app/base/views.py
+++ chat_app/base/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn Python'},
-    {'id': 2, 'name': 'Lets teach Django'},
-    {'id': 3, 'name': 'Front end questions'},
-    {'id': 4, 'name': 'Why learn devOps'},
-]
+from . import models
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Lets teach Django'},
+#     {'id': 3, 'name': 'Front end questions'},
+#     {'id': 4, 'name': 'Why learn devOps'},
+# ]
+
+
 
 # Create your views here.
 def home(request):
+    rooms = models.Room.objects.all()
     context = {
         'rooms': rooms
     }
     return render(request, 'base/home.html', context=context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    return render(request, 'base/room.html', context={'room': room})
+    room = models.Room.objects.get(pk=pk)
+    context = {
+        'room': room,
+    }
+    return render(request, 'base/room.html', context=context)
